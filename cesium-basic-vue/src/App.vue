dfbf92154ff35e437e26f5e2bd7dc6127d59ff09
“muddyrain” <qiu3291002845@gmail.com>
2023-04-10 18:05:23 +0800

    feat: 自定义添加地形

--- cesium-basic-vue/src/App.vue
+++ cesium-basic-vue/src/App.vue
@@ -24,75 +24,11 @@ onMounted(() => {
   const viewer = new Cesium.Viewer("cesiumContainer", {
     // 是否显示信息
     infoBox: false,
-    // 地理编码器 搜索框
-    geocoder: false,
-    // 是否显示home按钮
-    homeButton: false,
-    // 是否显示 2d 3d切换模式按钮
-    sceneModePicker: false,
-    // 是否显示图形选择器
-    baseLayerPicker: false,
-    // 是否显示帮助按钮
-    navigationHelpButton: false,
-    // 是否播放动画控制
-    animation: false,
-    // 是否显示时间轴
-    timeline: false,
-    // 是否显示全屏按钮
-    fullscreenButton: false,
-    // 设置天空盒子
-    skyBox: new Cesium.SkyBox({
-      sources: {
-        positiveX: './texture/sky/px.jpg',
-        negativeX: './texture/sky/nx.jpg',
-        positiveY: './texture/sky/ny.jpg',
-        negativeY: './texture/sky/py.jpg',
-        positiveZ: './texture/sky/pz.jpg',
-        negativeZ: './texture/sky/nz.jpg'
-      }
-    }),
-    // 天地图矢量路径图
-    // imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-    //   url: "http://t0.tianditu.com/vec_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=vec&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=11cf2ab5e7f08024032b86b7a93356ee",
-    //   layer: "tdtBasicLayer",
-    //   style: "default",
-    //   format: "image/jpeg",
-    //   tileMatrixSetID: "GoogleMapsCompatible",
-    // }),
-    //   天地图影像服务
-    imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-      url: "http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=11cf2ab5e7f08024032b86b7a93356ee",
-      layer: "tdtBasicLayer",
-      style: "default",
-      format: "image/jpeg",
-      tileMatrixSetID: "GoogleMapsCompatible",
-    }),
-    // OSM地图,
-    // imageryProvider: new Cesium.OpenStreetMapImageryProvider({
-    //   url: "https://a.tile.openstreetmap.org/",
-    // }),
-    // 高德矢量地图,
-    // imageryProvider: new Cesium.UrlTemplateImageryProvider({
-    //   url: "http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}",
-    //   layer: "tdtVecBasicLayer",
-    //   style: "default",
-    //   format: "image/png",
-    //   tileMatrixSetID: "GoogleMapsCompatible",
-    // }),
-  })
-
-  // 地图叠加
-  var imageryLayers = viewer.imageryLayers;
-  var layer = imageryLayers.addImageryProvider(
-    new Cesium.UrlTemplateImageryProvider({
-      url: "http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}",
-      layer: "tdtVecBasicLayer",
-      style: "default",
-      format: "image/png",
-      tileMatrixSetID: "GoogleMapsCompatible",
+    // 设置地形
+    terrainProvider: new Cesium.CesiumTerrainProvider({
+      url: "./terrains/gz"
     })
-  )
-  layer.alpha = 0.5
+  })
   // 隐藏 logo
   viewer.cesiumWidget.creditContainer.style.display = "none"
 })
