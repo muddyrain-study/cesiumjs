886cf7695be1baf38244269e4200441ef66b53ca
“muddyrain” <qiu3291002845@gmail.com>
2023-04-10 15:53:27 +0800

    基本基础设置

---
+++ cesium-basic-vue/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import * as Cesium from "cesium"
+import "cesium/Build/Cesium/Widgets/widgets.css";
+import { onMounted } from "vue";
+
+// 设置静态资源路径
+window.CESIUM_BASE_URL = '/'
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer("cesiumContainer", {
+  })
+})
+</script>
+
+<template>
+  <div id="cesiumContainer" ref="cesiumContainer"></div>
+</template>
+
+<style >
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
