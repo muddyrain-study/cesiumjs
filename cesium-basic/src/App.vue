df3aa63714ac60212a78ed9487b2bbcaa9cffb08
“muddyrain” <qiu3291002845@gmail.com>
2023-04-10 13:09:53 +0800

    feat: init

---
+++ cesium-basic/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="cesiumContaine" ref="cesiumContainer"></div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import * as Cesium from "cesium";
+const cesiumContainer = ref(null);
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
