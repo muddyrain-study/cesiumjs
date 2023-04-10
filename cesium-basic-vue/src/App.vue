52ac60e17474241d169a46b6d05c410f677e0380
“muddyrain” <qiu3291002845@gmail.com>
2023-04-10 16:54:32 +0800

    feat: cesium天空盒

--- cesium-basic-vue/src/App.vue
+++ cesium-basic-vue/src/App.vue
@@ -22,7 +22,35 @@ Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
 
 onMounted(() => {
   const viewer = new Cesium.Viewer("cesiumContainer", {
-    infoBox: false
+    // 是否显示信息
+    infoBox: false,
+    // 地理编码器 搜索框
+    geocoder: false,
+    // 是否显示home按钮
+    homeButton: false,
+    // 是否显示 2d 3d切换模式按钮
+    sceneModePicker: false,
+    // 是否显示图形选择器
+    baseLayerPicker: false,
+    // 是否显示帮助按钮
+    navigationHelpButton: false,
+    // 是否播放动画控制
+    animation: false,
+    // 是否显示时间轴
+    timeline: false,
+    // 是否显示全屏按钮
+    fullscreenButton: false,
+    // 设置天空盒子
+    skyBox: new Cesium.SkyBox({
+      sources: {
+        positiveX: './texture/sky/px.jpg',
+        negativeX: './texture/sky/nx.jpg',
+        positiveY: './texture/sky/ny.jpg',
+        negativeY: './texture/sky/py.jpg',
+        positiveZ: './texture/sky/pz.jpg',
+        negativeZ: './texture/sky/nz.jpg'
+      }
+    })
   })
   // 隐藏 logo
   viewer.cesiumWidget.creditContainer.style.display = "none"
